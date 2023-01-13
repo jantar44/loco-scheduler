e24ace006e1cd3bf7a7a45cc76ee31e7a2384730
Jan T <jan@op.pl>
2023-01-13 14:46:44 +0100

    added immovable courses logic + fixes

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-import pandas as pd
+from datetime import datetime, time
 
 class Course:
 
@@ -11,12 +10,19 @@ class Course:
         self.start_time = datetime.strptime(line[2].strip(),'%H:%M')
         self.end_time =  datetime.strptime(line[3].strip(),'%H:%M')
         self.duration = self.end_time - self.start_time
+        self.assigned = False
 
-    def __str__(self):
-        return f'Course({self.name})'
+    def __str__(self) -> str:
+        return f'{self.name}'
 
     def __repr__(self) -> str:
         return f'{self.name[0:3]}'
+
+    def get_count(self):
+        return self.count
+
+    def is_assigned(self):
+        return self.assigned
 
 class Scheduler:
 
@@ -44,35 +50,69 @@ class Scheduler:
                 courses_names[course.name] += 1
         for course in self.courses:
             course.count = courses_names[course.name]
+        self.courses
+
+    def sort_courses_by_number(self):
+        self.courses.sort(key = lambda x: x.get_count())
 
     @staticmethod
-    def weekdays(day:str):
-        weekdays =	{
-            'monday': 0,
-            'tuesday': 1,
-            'wednesday': 2,
-            'thursday': 3,
-            'friday':4,
-            'saturday':5,
-            'sunday':6
-            }
+    def weekdays_to_int(day:str) -> int:
+        weekdays =	{'monday':0, 'tuesday':1, 'wednesday':2, 'thursday':3, 'friday':4, 'saturday':5, 'sunday':6}
         return weekdays[day.strip().lower()]
 
-    def course_time(self, start_time, end_time):
-        start_int = int(start_time.hour-self.beginning*4 + start_time.minute/15)
-        duration = int((end_time - start_time).total_seconds()/60/15)
-        return start_int, start_int + duration
+    @staticmethod
+    def int_to_weekdays(day:int) -> str:
+            weekdays =	{0:'monday', 1:'tuesday', 2:'wednesday', 3:'thursday', 4:'friday', 5:'saturday', 6:'sunday'}
+            return weekdays[day]
 
-    def insert_to_schedule(self):
-        #first put immovable
+    def get_slot_of_time(self, time:datetime) -> int:
+        slot = int((time.hour-self.beginning)*4 + time.minute/15)
+        return slot
+
+    def get_time_of_slot(self, slot:int) -> str:
+        hour = slot//4 + self.beginning
+        min = 15*slot%4
+        f"{min:02}"
+        return '{}:{}'.format(f"{hour:02}", f"{min:02}")
+
+    # def slot_not_occupied(self, slot, day, hour):
+    #     if slot != 0:
+    #         print('In {} at {} there is already a course.'.format(day.capitalize(), self.get_time_of_slot(hour)))
+    #         return False
+    #     else:
+    #         return True
+
+    def reset_slot(self, slots:tuple, day:int):
+        for time in range(slots[0], slots[1]):
+            self.week[day][time] = 0
+
+    def set_slot(self, slots:list, day:int, course:Course):
+        for time in range(slots[0], slots[1]):
+            self.week[day][time] = course
+
+    def unmovable_to_schedule(self):
+        self.sort_courses_by_number()
         for course in self.courses:
-            if course.count == 1:
-                for hour in range(self.course_time(course.start_time, course.end_time)[0],self.course_time(course.start_time, course.end_time)[1]):
-                    self.week[self.weekdays(course.day_of_week)][hour] = course
+            slots = []
+            if course.get_count() == 1:
+                start_time, duration = self.get_slot_of_time(course.start_time), int(course.duration.total_seconds()/60/15)
+                for hour in range(start_time, start_time + duration):
+                    if self.week[Scheduler.weekdays_to_int(course.day_of_week)][hour] == 0:
+                        slots.append((course,hour))
+                    else:
+                        break
+
+                if len(slots) == course.duration.total_seconds()/60/15:
+                    self.set_slot([slots[0][1],slots[-1][1]], Scheduler.weekdays_to_int(course.day_of_week), slots[0][0])
+                    course.assigned = True
 
     def get_value(self):
         for day in self.week:
             print(day)
+
+
+
+
         # print(self.week)
         # print('self.courses_names ', self.courses)
 
@@ -81,4 +121,3 @@ class Scheduler:
         # time_start = course.start_time.hour*4 + course.start_time.minute/15
         # duration = course.duration
         # print((duration))
-
